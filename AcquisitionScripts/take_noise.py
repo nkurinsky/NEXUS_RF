f30259ff1070a5e7a480f237dd4937d9257b35c9
dylan <temples@u.northwestern.edu>
2021-12-20 10:08:03 -0600

    simplify, add comments, use numpy consistently

--- AcquisitionScripts/take_noise.py
+++ AcquisitionScripts/take_noise.py
@@ -1,152 +1,158 @@
-from __future__ import division
-import sys,os
-try:
-    import pyUSRP as u
-except ImportError:
-    try:
-        sys.path.append('../../Devices/GPU_SDR')
-        import pyUSRP as u
-    except ImportError:
-        print("Cannot find the pyUSRP package")
-
+## Import the relevant modules
+import sys, os
 import numpy as np
 import time
-import PyMKID_USRP_functions as puf
-import PyMKID_USRP_import_functions as puf2
 import h5py
 
+from __future__ import division
+
+## Try to read in the USRP modules
+## Exit out if you can't after adjusting path
+try:
+    import pyUSRP as u
+    import PyMKID_USRP_functions as puf
+    import PyMKID_USRP_import_functions as puf2
+except ImportError:
+    try:
+        sys.path.append('../DeviceControl/GPU_SDR')
+        import pyUSRP as u
+        import PyMKID_USRP_functions as puf
+        import PyMKID_USRP_import_functions as puf2
+    except ImportError:
+        print("Cannot find the pyUSRP package")
+        exit()
+
+## Attempt to connect to GPU SDR server
 if not u.Connect():
     u.print_error("Cannot find the GPU server!")
-    exit()
-
-
-rate = 200e6
+    
+## Set some noise scan parameters
+rate    = 200e6
 tx_gain = 0
 rx_gain = 17.5
-LO = 4.2e9
-res = 4.244905
-tracking_tones = [4.235e9,4.255e9]
+LO      = 4.2e9
+res     = 4.244905
+tracking_tones = n.array([4.235e9,4.255e9])
 
-powers = [-70.-65,-60,-55,-50,-45,-40,-35,-30,-25,-20,-15,-10]
-# powers = [-10]
-calibration_deltas = [-0.05, 0, 0.05]
+## Set the stimulus powers to loop over
+powers = n.arange(start = -70,
+                  stop  = - 5,
+                  step  =   5)
+n_pwrs = len(powers)
 
-frequencies = []
-means = []
+## Set the deltas to scan over in calibrations
+cal_deltas = n.linspace(start=-0.05, stop=0.05, num=3)
+n_c_deltas = len(cal_deltas)
 
+# powers = [-70.-65,-60,-55,-50,-45,-40,-35,-30,-25,-20,-15,-10]
 
-for power in powers:
+## Create some output containers
+## Each entry in these arrays is itself an array
+freqs = n.zeros(n_pwrs, dtype=object)
+means = n.zeros(n_pwrs, dtype=object)
 
+## Loop over the powers considered
+for i in n.arange(n_pwrs):
+    ## Pick this power
+    power = powers[i]
+
+    ## Ensure the power doesn't go above -25 dBm
     if power > -25:
         USRP_power = -25
-        tx_gain = power - USRP_power
+        tx_gain    = power - USRP_power
     else:
         USRP_power = power
 
-    N_power = 10**(((-1*USRP_power)-14)/20)
-    print( round(-14-20*np.log10(N_power),2),' dBm')
+    ## Calculate some derived quantities
+    N_power = n.power(10.,(((-1*USRP_power)-14)/20.))
+    pwr_clc = n.round(-14-20*np.log10(N_power),2)
+
+    ## Print some diagnostic text
+    print( pwr_clc,' dBm')
     print( N_power)
 
+    ## Do a VNA run with the USRP
+    vna_file, delay = puf2.vna_run(tx_gain        = tx_gain , 
+                                   rx_gain        = rx_gain ,
+                                   iter           = 1       ,
+                                   rate           = rate    ,
+                                   freq           = LO      ,
+                                   front_end      = 'A'     ,
+                                   f0             = 43e6    ,
+                                   f1             = 48e6    ,
+                                   lapse          = 20      ,
+                                   points         = 1e5     ,
+                                   ntones         = N_power ,
+                                   delay_duration = 0.1     ,
+                                   delay_over     = 'null'  )
 
-    vna_file, delay = puf2.vna_run(tx_gain=tx_gain, \
-                                   rx_gain = rx_gain,\
-                                   iter=1,\
-                                   rate=rate,
-                                   freq=LO,\
-                                   front_end='A',\
-                                   f0=43e6,f1=48e6,\
-                                   lapse=20,\
-                                   points=1e5,\
-                                   ntones=N_power,\
-                                   delay_duration=0.1,\
-                                   delay_over='null')
-
+    ## Fit the data acquired in this noise scan
     fs, qs, _,_,_,_,_ = puf.vna_file_fit(vna_file + '.h5',[res],show=False)
 
+    ## Extract the important parameters from fit
     f = fs[0]*1e9
     q = qs[0]
 
-    cal_iter = 0
-    calibration_frequencies = []
-    calibration_means = []
-    for delta in calibration_deltas:
-        readout_tones = [f + delta*float(f)/q] + tracking_tones
+    ## Create some output objects
+    ## Each entry is a single number
+    cal_freqs = n.zeros(n_c_deltas)
+    cal_means = n.zeros(n_c_deltas)
+
+    ## For each power, loop over all the calibration offsets
+    for j in n.arange(n_c_deltas):
+        delta = cal_deltas[j]
+
+        readout_tones  = (f + delta*float(f)/q) + tracking_tones
+        n_ro_tones     = len(readout_tones)
         # readout_tones = [tracking_tone_1, tracking_tone_2]
 
-        amplitudes = [1./N_power for x in readout_tones]
+        amplitudes     = 1./N_power * n.zeros(n_ro_tones)
 
+        relative_tones = n.zeros(n_ro_tones)
+        for k in n.arange(n_ro_tones):
+            relative_tones[k] = float(readout_tones[k]) - LO
 
-        relative_tones = []
-        for readout_tone in readout_tones:
-            relative_tone = float(readout_tone) - LO
-            relative_tones.append(relative_tone)
+        ## Another parameter for the noise run
+        duration = 50
 
-
-
-##        noise_file = puf2.noise_run(rate=200e6,\
-##                                    freq=LO,\
-##                                    front_end="A",\
-##                                    tones=np.asarray(relative_tones),\
-##                                    lapse=1,\
-##                                    decimation=40,\
-##                                    tx_gain=tx_gain,\
-##                                    rx_gain=rx_gain,\
-##                                    vna=None,\
-##                                    mode="DIRECT",\
-##                                    pf=4,\
-##                                    trigger=None,\
-##                                    amplitudes=amplitudes,\
-##                                    delay=delay*1e9)
-##
-##        _,transmission_means = puf.avg_noi(noise_file+'.h5',time_threshold=0.5)
-##        res_transmission = abs(transmission_means[0])
-##        tracking_transmission = abs(transmission_means[1])
-##        res_tracking_ratio = res_transmission / tracking_transmission
-##        amplitudes = [1./N_power, 1./N_power*res_tracking_ratio]
-##        os.remove(noise_file + '.h5')
-
-        if delta != 0:
-            duration = 50
-        else:
-            duration = 50
-
-        noise_file = puf2.noise_run(rate=200e6,\
-                                    freq=LO,\
-                                    front_end="A",\
-                                    tones=np.asarray(relative_tones),\
-                                    lapse=duration,\
-                                    decimation=40,\
-                                    tx_gain=tx_gain,\
-                                    rx_gain=rx_gain,\
-                                    vna=None,\
-                                    mode="DIRECT",\
-                                    pf=4,\
-                                    trigger=None,\
-                                    amplitudes=amplitudes,\
-                                    delay=delay*1e9)
+        ## Do a noise run with the USRP
+        noise_file = puf2.noise_run(rate       = rate       ,
+                                    freq       = LO         ,
+                                    front_end  = "A"        ,
+                                    tones      = np.array(relative_tones),
+                                    lapse      = duration   ,
+                                    decimation = 40         ,
+                                    tx_gain    = tx_gain    ,
+                                    rx_gain    = rx_gain    ,
+                                    vna        = None       ,
+                                    mode       = "DIRECT"   ,
+                                    pf         = 4          ,
+                                    trigger    = None       ,
+                                    amplitudes = amplitudes ,
+                                    delay      = delay*1e9  )
+        ## Add an extension to the file path
         noise_file += '.h5'
 
         time_threshold = duration / 2
 
-
         frequencies_scanned, noise_mean_scanned = puf.avg_noi(noise_file,time_threshold=time_threshold)
-        frequency = frequencies_scanned[0]
-        noise_mean = noise_mean_scanned[0]
 
-        calibration_frequencies.append(frequency)
-        calibration_means.append(noise_mean)
+        ## Store the result in the internal output arrays
+        cal_freqs[j] = frequencies_scanned[0]
+        cal_means[j] = noise_mean_scanned[0]
 
         if delta != 0:
             os.remove(noise_file)
 
-        cal_iter += 1
+    ## Store the resulting array from the inner loop in the
+    ## overall output containers (arrays of arrays)
+    freqs[i] = cal_freqs
+    means[i] = cal_means
 
-    frequencies.append(calibration_frequencies)
-    means.append(calibration_means)
-
+## Create an output container
 with h5py.File('noise_averages.h5','a') as fyle:
-    fyle.create_dataset('frequencies',data = np.asarray(frequencies))
-    fyle.create_dataset('means',data=np.asarray(means))
+    fyle.create_dataset('freqs',data=freqs)
+    fyle.create_dataset('means',data=means)
 
-
+## Disconnect from the USRP server
 u.Disconnect()
