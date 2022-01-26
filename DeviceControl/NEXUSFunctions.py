fa73004b27109928665d905351fa2874f8b7bbf8
nexus-arion <nkurinsky91@gmail.com>
2022-01-26 17:00:28 -0600

    add a new class for pulling temps from fridge not PID box

--- DeviceControl/NEXUSFunctions.py
+++ DeviceControl/NEXUSFunctions.py
@@ -54,3 +54,50 @@ class NEXUSFridge:
         
         f,v = self._sendCmd("1;2;"+str(spf))
         return
+
+class NEXUSTemps:
+
+    def __init__(self,server_ip="192.168.0.34",server_port=11034):
+        self.server_address = (server_ip, server_port)
+
+    def _sendCmd(self,cmd,getResponse=True,sleepTime=0.05):
+        cmdStr = cmd+"\n"
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect(self.server_address)
+            s.settimeout(1)
+            s.sendall(cmdStr.encode())
+            if(getResponse):
+                data = s.recv(1024)
+                retStr = data.decode()
+
+        retSplit = retStr.split("\n")
+        retVals = retSplit[1:-1]
+        retFlags = retSplit[0]
+
+        sleep(sleepTime)
+        if (getResponse):
+            return retFlags,retVals
+
+    def testConnection(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            try:
+                s.connect(self.server_address)
+            except ConnectionRefusedError:
+                print("ERROR -- Connection refused by remote host")
+            except OSError:
+                print("ERROR -- No route to host, check IP address")
+            else:
+                print("Connection OK")
+
+    def getVersion(self):
+        f,v = self._sendCmd("2;8");
+        return v[0];
+
+    def _getVar(self,var):
+        f,v = self._sendCmd("2;7;"+str(var))
+        vStr = v[0]
+        return vStr[4:]
+
+    def getTemp(self):
+        return self._getVar(3)
+
