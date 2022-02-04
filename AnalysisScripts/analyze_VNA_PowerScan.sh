649d21a13fde00524cdc6e79402cffa8b53f467c
dylan <temples@u.northwestern.edu>
2022-02-03 20:31:49 -0600

    add file

---
+++ AnalysisScripts/analyze_VNA_PowerScan.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+## Define the range of scans you want to process
+day_min=20220131
+tme_min=000000
+
+day_max=20220204
+tme_max=000000
+
+day=$day_min
+
+counter=1
+while [ $day -le $day_max ]
+do
+	echo "Day" /data/PowerSweeps/VNA/$day
+	((day++))
+done
