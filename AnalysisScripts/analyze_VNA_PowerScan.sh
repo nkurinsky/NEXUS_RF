04f147690ddaf6dd092ab163a111045e6d7ea24e
dylan <temples@u.northwestern.edu>
2022-02-03 21:16:00 -0600

    testing

--- AnalysisScripts/analyze_VNA_PowerScan.sh
+++ AnalysisScripts/analyze_VNA_PowerScan.sh
@@ -4,8 +4,8 @@
 day_min=20220131
 tme_min=000000
 
-day_max=20220204
-tme_max=000000
+day_max=20220131
+tme_max=235959
 
 day=$day_min
 
@@ -25,16 +25,24 @@ do
 	do
 		sers=$(echo $series | grep -E -o '([0-9])+\_([0-9])+')
 		time="${series: -6}"
-		# echo $sers
-		if [ "$day" -ge "$day_min" ]; then
-			if [ "$day" -le "$day_max" ]; then
-				echo $sers
-				echo $time
-				# if [ "$time" -ge "$tme_min" ]; then
-				# 	if [ "$time" -le "$tme_max" ]; then
-				# 		echo sers
-				# 	fi
-				# fi
+		
+		## Handle the edge cases first
+		if [ "$day" = "$day_min" ]; then
+			if [ "$time" -ge "$tme_min" ]; then
+				python plot_VNA_PowerScan.py -s $sers
+			fi
+		fi
+
+		if [ "$day" = "$day_max" ]; then
+			if [ "$time" -le "$tme_max" ]; then
+				python plot_VNA_PowerScan.py -s $sers
+			fi
+		fi
+
+		# Handle the middling cases
+		if [ "$day" -gt "$day_min" ]; then
+			if [ "$day" -lt "$day_max" ]; then
+				python plot_VNA_PowerScan.py -s $sers
 			fi
 		fi
 	done
