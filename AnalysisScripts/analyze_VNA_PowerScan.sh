ece2504ea6e57426fe6f55b4ffea77b6517642ee
dylan <temples@u.northwestern.edu>
2022-02-03 20:59:34 -0600

    testing

--- AnalysisScripts/analyze_VNA_PowerScan.sh
+++ AnalysisScripts/analyze_VNA_PowerScan.sh
@@ -19,9 +19,17 @@ day=$day_min
 for day_path in /data/PowerSweeps/VNA/*
 do
 	echo $day_path
+	day=(echo $day_path) | grep -E -o '([0-9])+'
 	for series in $day_path/*
 	do
 		echo $series | grep -E -o '([0-9])+\_([0-9])+'
+		time="${series: -6}"
+		echo time
+		if [ "$day" -ge "$day_min" ]
+			if [ "$day" -le "$day_min" ]
+				echo GOOD
+			fi
+		fi
 	done
 done
 
