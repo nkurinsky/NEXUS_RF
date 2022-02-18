c89c75147e69c41573b0444d5cc7071c1959b6ba
dylan <temples@u.northwestern.edu>
2022-02-17 20:10:55 -0600

    add new gui files

---
+++ DeviceControl/LaserDriver/laser_driver.py
@@ -0,0 +1,206 @@
+#!/usr/bin/env/python
+# laser_driver.py
+
+import sys
+import glob
+import serial
+import time
+
+def pw_to_freq(pw_us):
+	## Convert a "pulse width" in microseconds
+	## to a "frequency" in MHz, assuming 50% duty cycle
+	return 1./(2.*pw_us)
+
+def freq_to_pw(fq_MHz):
+	## Convert a "frequency" in MHz
+	## to a "pulse width" in microseconds, assuming 50% duty cycle
+	return 1./(2.*fq_Hz)
+
+def bool2yn(val):
+    return 'y' if val else 'n'
+
+def yn2bool(val):
+    return val.lower()=='y'
+
+def serial_ports():
+    """ Lists serial port names
+    :raises EnvironmentError:
+    On unsupported or unknown platforms
+    :returns:
+    A list of the serial ports available on the system
+    """
+    if sys.platform.startswith('win'):
+        ports = ['COM%s' % (i + 1) for i in range(256)]
+    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+        # this excludes your current terminal "/dev/tty"
+        ports = glob.glob('/dev/tty[A-Za-z]*')
+    elif sys.platform.startswith('darwin'):
+        ports = glob.glob('/dev/tty.*')
+    else:
+        raise EnvironmentError('Unsupported platform')
+    result = []
+    for port in ports:
+        try:
+            s = serial.Serial(port)
+            s.close()
+            result.append(port)
+        except (OSError, serial.SerialException):
+            pass
+    return result
+
+class LaserDriver():
+
+	ArduinoSerial = None  # serial connection object
+	serial_port   = None  # string pointing to serial port address
+
+	def __init__(self):
+		## Instantiate the serial connection object
+		self.ArduinoSerial = serial.Serial() # ('/dev/ttyUSB0', 115200, timeout=.1)
+		time.sleep(0.5)
+
+		#-- Looking for serial ports available
+		serial_ports_available = serial_ports()
+		try:
+			self.serial_port = serial_ports_available[0]
+		except IndexError:
+			print("No serial ports available, exiting")
+			quit()
+		return
+
+	def _sendCmd(self, cmd):
+		#print('Sending command to Arduino: %s'%cmd)
+		self.ArduinoSerial.write(str.encode(cmd+'\n'))
+		return
+
+	def _getReply(self):
+		line = "No response."
+		try:
+			line = self.ArduinoSerial.readline()
+		except Exception as e:
+		    print ("Error in reply: "+str(e))
+		#print(line)
+		return line.decode('utf-8').strip()
+
+	def configure_serial(self):
+		self.ArduinoSerial.port     = self.serial_port
+		self.ArduinoSerial.baudrate = 115200
+		self.ArduinoSerial.timeout  = 0.5
+		try:
+			self.ArduinoSerial.open()
+		except Exception as e:
+			print("Error opening serial port: "+str(e))
+		return
+	
+	def get_identity(self):
+		self._sendCmd("*IDN?")
+		time.sleep(0.05)
+		return self._getReply()
+
+	def led(self, state=False):
+		cmd = "*LED" + ("1" if state else "0")
+		self._sendCmd(cmd)
+		time.sleep(0.05)
+		print(self._getReply())
+		return
+
+	def set_pw(self, pw_us="10.00"):
+		try:
+			pw = float(pw_us)
+		except Exception as e:
+			print("Error converting",pw_us,"to float -- ",str(e))
+			return
+
+		clock_f = pw_to_freq(pw)
+
+		if (clock_f < 0.001):
+			clock_f = 0.001
+
+		if (clock_f > 99.99):
+			clock_f = 99.99
+
+		try:
+			cmd = "F%5.2f"%clock_f
+		except Exception as e:
+			print("Error converting",clock_f,"to string -- ",str(e))
+			return
+
+		self._sendCmd(cmd)
+		time.sleep(0.05)
+		print(self._getReply())
+		return
+
+	def get_pw(self):
+		self._sendCmd("*F?")
+		time.sleep(0.05)
+		pw_MHz = float(self._getReply())
+		return string(freq_to_pw(pw_MHz))
+
+	def set_bf(self, bf_Hz="250.0"):
+		try:
+			bf = float(bf_Hz)
+		except Exception as e:
+			print("Error converting",bf_Hz,"to float -- ",str(e))
+			return
+
+		if (bf < 5.0):
+			bf = 5.0
+
+		if (bf > 999.9):
+			bf = 999.9
+
+		try:
+			cmd = "B%5.2f"%bf
+		except Exception as e:
+			print("Error converting",bf,"to string -- ",str(e))
+			return
+
+		self._sendCmd(cmd)
+		time.sleep(0.05)
+		print(self._getReply())
+		return
+
+	def get_bf(self):
+		self._sendCmd("*B?")
+		time.sleep(0.05)
+		return self._getReply()
+
+	def set_lr(self, lr_R="127"):
+		try:
+			R = int(lr_R)
+		except Exception as e:
+			print("Error converting",lr_R,"to float -- ",str(e))
+			return
+
+		if (R < 1):
+			R = 1
+
+		if (R > 127):
+			R = 127
+
+		try:
+			cmd = "R%d"%R
+		except Exception as e:
+			print("Error converting",R,"to string -- ",str(e))
+			return
+
+		self._sendCmd(cmd)
+		time.sleep(0.05)
+		print(self._getReply())
+		return
+
+	def get_lr(self):
+		self._sendCmd("*R?")
+		time.sleep(0.05)
+		return self._getReply()
+
+	def enable_laser(self, state=False):
+		cmd = "ON" if state else "OFF"
+		self._sendCmd(cmd)
+
+	def close(self):
+		self.led(False)
+		try:
+			self.ArduinoSerial.close() # close serial port
+		except:
+			pass
+		quit()
---
+++ DeviceControl/LaserDriver/start_gui.py
@@ -0,0 +1,177 @@
+#!/usr/bin/env/python
+# start_gui.py
+
+try:
+    import tkinter as tk
+except ImportError:
+    import Tkinter as tk
+
+from laser_driver import *
+
+## Create a driver instance
+driver = LaserDriver()
+
+## Geometry of the GUI
+win_size_x  = 500
+win_size_y  = 200
+
+but_size_x  =  12
+but_size_y  =   2
+
+but1_pos_x  =   0
+but1_pos_y  =  70
+
+but2_pos_x  =   0
+but2_pos_y  = 100
+
+## Create the main window and storing the window object in 'win'
+win=Tk() 
+
+## Set the title of the window
+win.title('NEXUS Laser Contol')
+
+## Set the size of the window
+win.geometry( str(int(win_size_x)) + "x" + str(int(win_size_y)) )
+
+## ========= BUTTONS ========= ##
+
+## Details of the "configure serial" button
+btn_cfg_serial=Button(win, text="Configure\nSerial Port", 
+    width   = but_size_x,
+    height  = but_size_y,
+    state   = "normal", )
+btn_cfg_serial.grid(row=0, column=0)
+
+## Details of the "roll call" button
+btn_roll_call=Button(win, text="Roll Call", 
+    width   = but_size_x,
+    height  = but_size_y,
+    state   = "disabled", )
+btn_roll_call.grid(row=1, column=0)
+
+## Details of the "--" button
+btn_led_on=Button(win, text="LED On", 
+    width   = but_size_x,
+    height  = but_size_y,
+    state   = "disabled", )
+btn_led_on.grid(row=2, column=0)
+
+## Details of the "--" button
+btn_led_off=Button(win, text="LED Off", 
+    width   = but_size_x,
+    height  = but_size_y,
+    state   = "disabled", )
+btn_led_off.grid(row=3, column=0)
+
+## Details of the "Update PW" button
+btn_update_pw=Button(win, text="Update", 
+    width   = int(but_size_x/2),
+    height  = int(but_size_y/2),
+    state   = "disabled", )
+btn_update_pw.grid(row=1, column=3)
+
+## Details of the "Update BF" button
+btn_update_bf=Button(win, text="Update", 
+    width   = int(but_size_x/2),
+    height  = int(but_size_y/2),
+    state   = "disabled", )
+btn_update_bf.grid(row=2, column=3)
+
+## Details of the "Update BF" button
+btn_update_lr=Button(win, text="Update", 
+    width   = int(but_size_x/2),
+    height  = int(but_size_y/2),
+    state   = "disabled", )
+btn_update_lr.grid(row=3, column=3)
+
+## ========= LABELS ========= ##
+Label(win, text='Roll Call: ').grid(row=0, column=1)
+Label(win, text='Pulse Width [us]:').grid(row=1, column=1)
+Label(win, text='Burst Freq. [Hz]:').grid(row=2, column=1)
+Label(win, text='  Laser R. [int]:').grid(row=3, column=1)
+
+## ========= TEXT BOXES ========= ##
+## Create containers for the entry strings
+id_str = StringVar() ; id_str.set("Identity")
+pw_str = StringVar() ; pw_str.set("PulseWidth")
+bf_str = StringVar() ; bf_str.set("BurstFreq")
+lr_str = StringVar() ; lr_str.set("LaserR")
+
+e_id = Entry(win, textvariable=id_str, state="disabled")
+e_id.grid(row=0, column=2)
+
+e_pw = Entry(win, textvariable=pw_str, state="disabled")
+e_pw.grid(row=1, column=2)
+
+e_bf = Entry(win, textvariable=bf_str, state="disabled")
+e_bf.grid(row=2, column=2)
+
+e_lr = Entry(win, textvariable=bf_str, state="disabled")
+e_lr.grid(row=3, column=2)
+
+## ======================================= ##
+
+def cfg_serial():
+    btn_cfg_serial["state"] = "disabled"
+    
+    driver.configure_serial()
+    
+    btn_roll_call["state"]  = "normal"
+
+    return
+btn_cfg_serial["command"] = cfg_serial
+
+def roll_call():
+    btn_roll_call["state"] = "disabled"
+
+    id_str.set( driver.get_identity() )
+    pw_str.set( driver.get_pw() )
+    bf_str.set( driver.get_bf() )
+    lr_str.set( driver.get_lr() )
+
+    btn_led_on["state"]    = "normal"
+    btn_led_off["state"]   = "normal"
+    btn_update_pw["state"] = "normal"
+    btn_update_bf["state"] = "normal"
+    btn_update_lr["state"] = "normal"
+    e_pw["state"] = "normal"
+    e_bf["state"] = "normal"
+    e_lr["state"] = "normal"
+
+    return
+btn_roll_call['command'] = roll_call
+
+def led_on():
+    btn_led_on["state"]  = "disabled"
+    driver.led(True)
+    btn_led_off["state"] = "normal"
+    return
+btn_led_on['command'] = led_on
+
+def led_off():
+    btn_led_off["state"] = "disabled"
+    driver.led(False)
+    btn_led_on["state"]  = "normal"
+    return
+btn_led_off['command'] = led_off
+
+def update_pw():
+    driver.set_pw( pw_str.get() )
+    pw_str.set( driver.get_pw() )
+    return
+btn_update_pw['command'] = update_pw
+
+def update_bf():
+    driver.set_bf( bf_str.get() )
+    bf_str.set( driver.get_bf() )
+    return
+btn_update_bf['command'] = update_bf
+
+def update_lr():
+    driver.set_lr( lr_str.get() )
+    lr_str.set( driver.get_lr() )
+    return
+btn_update_lr['command'] = update_lr
+
+## Execute
+win.mainloop() #running the loop that works as a trigger
