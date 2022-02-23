17c739f1beb3bb8692385020e65a2e49b0afde97
dylan <temples@u.northwestern.edu>
2022-02-23 12:14:15 -0600

    change sereis

--- AnalysisScripts/plot_VNA_PowerScan.py
+++ AnalysisScripts/plot_VNA_PowerScan.py
@@ -27,8 +27,8 @@ dfc = plt.rcParams['axes.prop_cycle'].by_key()['color']
 show_plots = False
 
 ## Series identifier
-day    = '20220127'
-time   = '160519'
+day    = '20220222'
+time   = '130249'
 series = day + '_' + time
 
 ## Path to VNA data
