0af7865776d01a1ac6b8f18c1c6c9d6f28308f43
nexus-arion <nkurinsky91@gmail.com>
2022-05-02 15:45:21 -0500

    adjust to temp scan settings

--- AcquisitionScripts/run_VNA_PowerScan_NEXUS.sh
+++ AcquisitionScripts/run_VNA_PowerScan_NEXUS.sh
@@ -4,14 +4,15 @@
 n_scans=1
 
 # Power range
-pmin=-50
-pmax=-20
+pmin=-55
+pmax=-15
 pstep=5
 
 # Frequency range
 # fmin=4.24195e9 ; fmax=4.24235e9	## Al -- 4.242175 G
 # fmin=4.242095e9 ; fmax=4.242265e9	## Al -- 4.242175 G, 170kHz range
 # fmin=4.242005e9 ; fmax=4.242355e9	## Al -- 4.242175 G, 350kHz range
+# fmin=4.241430e9 ; fmax=4.242930e9	## Al -- 4.242175 G, 1.5MHz range
 # fmin=3.93600e9 ; fmax=3.93680e9	## Nb 1
 # fmin=3.98280e9 ; fmax=3.98400e9	## Nb 2
 # fmin=4.02660e9 ; fmax=4.02760e9	## Nb 3
@@ -20,16 +21,17 @@ pstep=5
 # fmin=4.20258e9 ; fmax=4.20308e9	## Nb 6 -- 4.20283 G
 # fmin=4.24456e9 ; fmax=4.24496e9	## Nb 7 (next to resonator) -- 4.24476 G
 # fmin=4.244675e9 ; fmax=4.244845e9	## Nb 7 (next to resonator) -- 4.24476 G, 170kHz range
-fmin=4.244585e9 ; fmax=4.244935e9	## Nb 7 (next to resonator) -- 4.24476 G, 350kHz range
+# fmin=4.244585e9 ; fmax=4.244935e9	## Nb 7 (next to resonator) -- 4.24476 G, 350kHz range
+fmin=4.244010e9 ; fmax=4.245515e9	## Nb 7 (next to resonator) -- 4.24476 G, 1.5MHz range
 # fmin=4.29080e9 ; fmax=4.29140e9	## Nb 8
 # fmin=4.33050e9 ; fmax=4.33140e9	## Nb 9
 # fmin=4.37460e9 ; fmax=4.37630e9	## Nb 10
 
 # ns=100000
-ns=14000
+ns=15000
 
 # How many averages to do at each power
-na=3
+na=5
 
 counter=1
 while [ $counter -le $n_scans ]
