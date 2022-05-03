b1e8089c7eaad1ed53e698b2b52029747427e3b5
dylan <temples@u.northwestern.edu>
2022-05-03 11:44:29 -0500

    changes

--- AnalysisScripts/ResonanceFitter.py
+++ AnalysisScripts/ResonanceFitter.py
@@ -583,8 +583,8 @@ def sweep_fit(f, z, file_fit_obj, nsig=3, fwindow=5e-4, pdf_rewrite=False, addit
     ## Handle too many peaks
     if (len(peaklist) > 10):
         # peaklist = np.array([ np.argmax(20*np.log10(abs(np.array(z)))) ])
-        peaklist = np.array([ np.argmax(mfz) ])
-        # peaklist = np.array([ np.argmin(20*np.log10(np.array(z))) ])
+        # peaklist = np.array([ np.argmax(mfz) ])
+        peaklist = np.array([ np.argmin(20*np.log10(np.array(z))) ])
 
     ## add the manually entered frequencies to peaklist
     for added in additions:
