88e3cc2a23f618fcc3001ac7d67e890399dd3f47
dylan <temples@u.northwestern.edu>
2022-09-23 10:35:29 -0500

    debug

--- DeviceControl/NEXUSFunctions.py
+++ DeviceControl/NEXUSFunctions.py
@@ -202,6 +202,7 @@ def read_Lakeshore_data(date_series, offset):
     for iseries in date_series:
         iseries = iseries.replace("-","")
         data = pd.read_csv(os.path.join(local_path,"Temperature_"+iseries+".txt"), delimiter = ';') 
+        print(data.keys())
         data['ctime'] = [datetime.datetime.strptime(elem, '%Y-%m-%d %H:%M:%S.%f')+offset for elem in data['Time']]
         datalist.append(data)
     result = pd.concat(datalist)
