fix the noise time when determining averages

AcquisitionScripts/USRP_GetNoise_NEXUS.py
@@ -323,11 +323,14 @@ def runNoise(tx_gain, rx_gain, _iter, rate, freq, front_end, f0, f1, lapse_VNA, 
         print("Amplitudes:         ", amplitudes)
         print("LO Frequency [Hz]:  ", freq)
 
+        ## Determine how long to acquire noise
+        dur_noise = lapse_noise if ((np.abs(delta) < 0.005) and (cal_lapse_sec < lapse_noise)) else cal_lapse_sec  ## passed in sec
+
         print("Starting Noise Run...")
         ## Do a noise run with the USRP
         noise_file = u.get_tones_noise(relative_tones, 
                                     #measure_t  = lapse_noise,  ## passed in sec
-                                    measure_t  = lapse_noise if ((np.abs(delta) < 0.005) and (cal_lapse_sec < lapse_noise)) else cal_lapse_sec,  ## passed in sec
+                                    measure_t  = dur_noise,
                                     tx_gain    = tx_gain, 
                                     rx_gain    = rx_gain, 
                                     rate       = rate,  ## passed in Hz
@@ -347,7 +350,7 @@ def runNoise(tx_gain, rx_gain, _iter, rate, freq, front_end, f0, f1, lapse_VNA, 
         ## Add an extension to the file path
         noise_file += '.h5'
 
-        time_threshold = duration / 2
+        time_threshold = 0.3 * dur_noise # dur_noise / 2
 
         frequencies_scanned, noise_mean_scanned = puf.avg_noi(noise_file,time_threshold=time_threshold)
 