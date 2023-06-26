ef5e0c8066aa7e9cf54cfd9f442214ef151b723f
nexus-arion <nkurinsky91@gmail.com>
2023-06-26 09:14:51 -0500

    add comments, adjust parameters

--- AcquisitionScripts/run_VNA_PowerScan_NEXUS.sh
+++ AcquisitionScripts/run_VNA_PowerScan_NEXUS.sh
@@ -6,33 +6,32 @@ n_scans=1
 # Power range
 pmin=-50
 pmax=-15
-pstep=1
+pstep=5
 
 # Frequency range
 # fmin=4.24195e9 ; fmax=4.24235e9	## Al -- 4.242175 G
-fmin=4.242095e9 ; fmax=4.242265e9	## Al -- 4.242175 G, 170kHz range
+# fmin=4.242095e9 ; fmax=4.242265e9	## Al -- 4.242175 G, 170kHz range
 # fmin=4.242005e9 ; fmax=4.242355e9	## Al -- 4.242175 G, 350kHz range
 # fmin=4.241233e9 ; fmax=4.2427330e9	## Al -- 4.241983 G, 1.5MHz range
-# fmin=3.93600e9 ; fmax=3.93680e9	## Nb 1
-# fmin=3.98280e9 ; fmax=3.98400e9	## Nb 2
-# fmin=4.02660e9 ; fmax=4.02760e9	## Nb 3
-# fmin=4.07240e9 ; fmax=4.07300e9	## Nb 4
-# fmin=4.11720e9 ; fmax=4.11780e9	## Nb 5
-# fmin=4.20258e9 ; fmax=4.20308e9	## Nb 6 -- 4.20283 G
-# fmin=4.20208e9 ; fmax=4.20358e9	## Nb 6 -- 4.20283 G, 1.5 MHz range
-# fmin=4.24456e9 ; fmax=4.24496e9	## Nb 7 (next to resonator) -- 4.24476 G
-# fmin=4.244468e9 ; fmax=4.244638e9	## Nb 7 (next to resonator) -- 4.244553 G, 170kHz range
-# fmin=4.244585e9 ; fmax=4.244935e9	## Nb 7 (next to resonator) -- 4.24476 G, 350kHz range
-# fmin=4.244010e9 ; fmax=4.245510e9	## Nb 7 (next to resonator) -- 4.24476 G, 1.5MHz range
-# fmin=4.29080e9 ; fmax=4.29140e9	## Nb 8
-# fmin=4.33050e9 ; fmax=4.33140e9	## Nb 9
-# fmin=4.37460e9 ; fmax=4.37630e9	## Nb 10
+
+# fmin=3.93545e9 ; fmax=3.93695e9	## Nb 1 -- 3.93620 G, 1.5 MHz range
+# fmin=3.98250e9 ; fmax=3.98400e9	## Nb 2 -- 3.98325 G, 1.5 MHz range
+# fmin=4.02605e9 ; fmax=4.02755e9	## Nb 3 -- 4.02680 G, 1.5 MHz range
+# fmin=4.07175e9 ; fmax=4.07325e9	## Nb 4 -- 4.07250 G, 1.5 MHz range
+# fmin=4.11650e9 ; fmax=4.11800e9	## Nb 5 -- 4.11725 G, 1.5 MHz range
+# fmin=4.20185e9 ; fmax=4.20335e9	## Nb 6 -- 4.20260 G, 1.5 MHz range
+# fmin=4.24380e9 ; fmax=4.24530e9	## Nb 7 -- 4.24455 G, 1.5 MHz range
+# fmin=4.28995e9 ; fmax=4.29145e9	## Nb 8 -- 4.29070 G, 1.5 MHz range
+# fmin=4.32995e9 ; fmax=4.33145e9	## Nb 9 -- 4.33070 G, 1.5 MHz range
+fmin=4.37455e9 ; fmax=4.37605e9	## Nb 10-- 4.37530 G, 1.5 MHz range
+# fmin=4.05400e9 ; fmax=4.05900e9	## Box mode
 
 # ns=100000
-ns=150000
+# ns=150000
+ns=20000
 
 # How many averages to do at each power
-na=20
+na=10
 
 counter=1
 while [ $counter -le $n_scans ]
