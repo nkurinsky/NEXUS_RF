6e2a064daccfdbfeba33df79c3674d0da3e43de6
dylan <temples@u.northwestern.edu>
2023-07-12 22:29:25 -0500

    typo

--- BackendTools/OptFilterTools.py
+++ BackendTools/OptFilterTools.py
@@ -382,7 +382,7 @@ def plot_all_pulse_windows(LED_files, noise_file, vna_file, p_params, p1=5, p2=9
     for pulse_file in LED_files:
 
         ## Save the cut criteria to our dictionaries
-        pulse_rqs[pulse_file] =  = plot_pulse_windows(
+        pulse_rqs[pulse_file] = plot_pulse_windows(
             pulse_file, noise_file, vna_file, p_params, 
             p1=p1, p2=p2, decimate_down_to=decimate_down_to, pulse_cln_dec=pulse_cln_dec, 
             PHASE=PHASE, show_plots=show_plots)
