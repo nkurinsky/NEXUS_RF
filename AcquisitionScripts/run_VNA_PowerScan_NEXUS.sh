88bd5a9ba126831e4def8eee8f5b82c854ffa427
Dylan Temples <dtemps123@gmail.com>
2023-10-02 16:15:09 -0500

    add freq params

--- AcquisitionScripts/run_VNA_PowerScan_NEXUS.sh
+++ AcquisitionScripts/run_VNA_PowerScan_NEXUS.sh
@@ -10,10 +10,13 @@ pstep=5
 ifbw=10000
 
 # Frequency range
-# fmin=4.24195e9 ; fmax=4.24235e9	## Al -- 4.242175 G
-# fmin=4.242095e9 ; fmax=4.242265e9	## Al -- 4.242175 G, 170kHz range
-# fmin=4.242005e9 ; fmax=4.242355e9	## Al -- 4.242175 G, 350kHz range
-# fmin=4.241233e9 ; fmax=4.2427330e9	## Al -- 4.241983 G, 1.5MHz range
+# fmin=4.6075e9 ; fmax=4.6275e9         ## FNAL-III @ 4.6175 G, 20 MHz range
+fmin=5.3864e9 ; fmax=5.3884e9         ## FNAL-III @ 5.3874 G, 2 MHz range
+
+# fmin=4.24195e9 ; fmax=4.24235e9       ## Al -- 4.242175 G
+# fmin=4.242095e9 ; fmax=4.242265e9     ## Al -- 4.242175 G, 170kHz range
+# fmin=4.242005e9 ; fmax=4.242355e9     ## Al -- 4.242175 G, 350kHz range
+# fmin=4.241233e9 ; fmax=4.2427330e9    ## Al -- 4.241983 G, 1.5MHz range
 
 # fmin=3.93545e9 ; fmax=3.93695e9	## Nb 1 -- 3.93620 G, 1.5 MHz range
 # fmin=3.98250e9 ; fmax=3.98400e9	## Nb 2 -- 3.98325 G, 1.5 MHz range
@@ -24,7 +27,7 @@ ifbw=10000
 # fmin=4.24380e9 ; fmax=4.24530e9	## Nb 7 -- 4.24455 G, 1.5 MHz range
 # fmin=4.28995e9 ; fmax=4.29145e9	## Nb 8 -- 4.29070 G, 1.5 MHz range
 # fmin=4.32995e9 ; fmax=4.33145e9	## Nb 9 -- 4.33070 G, 1.5 MHz range
-fmin=4.37455e9 ; fmax=4.37605e9	## Nb 10-- 4.37530 G, 1.5 MHz range
+# fmin=4.37455e9 ; fmax=4.37605e9	## Nb 10-- 4.37530 G, 1.5 MHz range
 # fmin=4.05400e9 ; fmax=4.05900e9	## Box mode
 
 # ns=100000
@@ -32,7 +35,7 @@ fmin=4.37455e9 ; fmax=4.37605e9	## Nb 10-- 4.37530 G, 1.5 MHz range
 ns=20000
 
 # How many averages to do at each power
-na=10
+na=15
 
 counter=1
 while [ $counter -le $n_scans ]
